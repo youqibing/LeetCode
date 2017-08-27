4ff7995acd30d00c6f8835a9eadc5cd411899f05
liuxuan_i <ranger@bingyan.net>
2017-08-28 02:33:45 +0800

    add RotateArray,there are some bugs yet

---
+++ src/Array/RotateProblem/RotateArray.java
@@ -0,0 +1,52 @@
+package Array.RotateProblem;
+
+/**
+ * Rotate an array of n elements to the right by k steps.
+ *
+ * For example, with n = 7 and k = 3, the array [1,2,3,4,5,6,7] is rotated to [5,6,7,1,2,3,4].
+ *
+ * Note:
+ * Try to come up as many solutions as you can, there are at least 3 different ways to solve this problem.
+ *
+ * Hint:
+ * Could you do it in-place with O(1) extra space?
+ *
+ * 
+ */
+public class RotateArray {
+    public static void main(String args[]){
+        int[] nums = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12};
+        //int[] nums = {12, 11, 10, 4, 5, 6, 7, 8, 9, 1, 2, 3};
+        int k =3;
+
+        rotateArray(nums,k);
+    }
+
+    private static void rotateArray(int[] numbers, int k){
+        int temp,j=k;
+
+        for(int i=0; i<numbers.length-1; i++){
+            //System.out.println("i: "+i);
+            if(k-i>0){
+                temp = numbers[i];
+                numbers[i] = numbers[numbers.length - k + i];
+                numbers[numbers.length - k + i] = temp;
+
+            }else {
+                for( ;j<numbers.length -1; j++){
+                    System.out.println(j);
+                    if(numbers[j]>numbers[j+1]){
+                        temp=numbers[j];
+                        numbers[j]=numbers[j+1];
+                        numbers[j+1]=temp;
+                    }
+                }
+            }
+
+        }
+
+        for(int i=0; i<numbers.length; i++){
+            System.out.print(numbers[i]+",");
+        }
+    }
+}
