eb205afd7d240651df042c5a6924a2e0d18326ee
liuxuan_i <ranger@bingyan.net>
2017-09-08 15:18:21 +0800

    add MergeIntervals

--- src/Array/Intervals/MergeIntervals.java
+++ src/Array/Intervals/MergeIntervals.java
@@ -1,4 +1,80 @@
 package Array.Intervals;
 
+import java.util.*;
+
+/**
+ * Given a collection of intervals, merge all overlapping intervals.
+ *
+ * For example,
+ * Given [1,3],[2,6],[8,10],[15,18],
+ * return [1,6],[8,10],[15,18].
+ *
+ * 数轴覆盖问题, 将各条线段的start按从小到大排序，然后依次比较每条线段的end，看是否被前一段覆盖即可。
+ * 这个题主要要掌握Comparator接口的用法, 非常有用！
+ */
 public class MergeIntervals {
+
+    public static void main(String[] args){
+
+        List<Interval> list = new ArrayList<>();
+
+        Interval i1 = new Interval(1,3);
+        Interval i2 = new Interval(2,6);
+        Interval i3 = new Interval(8,10);
+        Interval i4 = new Interval(15,18);
+
+        list.add(i1);
+        list.add(i2);
+        list.add(i3);
+        list.add(i4);
+
+        mergeIntervals(list);
+    }
+
+    private static void mergeIntervals(List<Interval> intervals){
+        if(intervals == null || intervals.size()<=1){
+            return ;
+        }
+
+        Collections.sort(intervals, new IntervalsComparator());
+
+        List<Interval> result = new ArrayList<>();
+        Interval last = intervals.get(0);
+
+        for(int i=1; i<intervals.size(); i++){  //注意i从1开始
+            Interval curt = intervals.get(i);
+            if(curt.start <= last.end){   //前一个(last)的end值改变,相当于把两个区间合并,此时last还是0,curr循环一次变为2，相当于第0个和
+                last.end = Math.max(last.end, curt.end); //第2个比较,此时直接执行下面的else并将last(合并区间后的第一个)添加到结果返回值中
+            }else {
+                result.add(last);
+                last = curt;    //指针后移一位,比较2和3
+            }
+            System.out.print("["+last.start+","+last.end+"]");
+        }
+
+        result.add(last);
+    }
+
+
+    static class IntervalsComparator implements Comparator<Interval>{
+
+        @Override
+        public int compare(Interval a, Interval b) {
+            return a.start - b.start;   //按照Interval类中 start 的升序排列
+        }
+    }
+
+
+
+
+    static class Interval {
+      int start;
+      int end;
+      Interval() {
+          start = 0; end = 0;
+      }
+      Interval(int s, int e) {
+          start = s; end = e;
+      }
+    }
 }
